a13406c11446b002a85e1257443a5fc6e79249f5
Laboratorio2 <jponc@hotamil.com>
2018-02-11 16:30:19 -0600

    agregando prueba

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Laboratorio2
- * Date: 11/02/2018
- * Time: 15:11
- */
-echo "hi"
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<header style="backgroud-color: blue; width: 100%; height: 200px;">
+    <section style="background-color:green width:100%; height: 50px;"></section>
+    <p> soy un parrafo negro </p>
+
+    <nav style="background-color:"></nav>
+</header>
+
+header>section+nav
+
+
+
+<section>
+
+</section style="color: blue>
+<p> hola </p>
+
